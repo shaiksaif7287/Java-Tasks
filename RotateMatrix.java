746f0d89e6cd443166c1107fe797a2af01f40fbb
shaiksaif7287 <dsaifshaik@gmail.com>
2025-06-18 23:51:01 +0530

    Add files via upload

---
+++ RotateMatrix.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class RotateMatrix {
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int left = 0;
+            int right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println("Rotated Matrix (90 degrees clockwise):");
+        printMatrix(matrix);
+    }
+}
